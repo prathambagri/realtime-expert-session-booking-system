0ae28fca4c08905e3e8f57383c1f224e007549eb
Pratham Bagri <prathambagri2017@gmail.com>
2026-07-24 04:27:26 +0530

    bugg issue resolve

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,7 +5,9 @@ const nodemailer = require("nodemailer");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
